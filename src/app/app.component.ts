a1091597b83071b13c0a581e5732aa18a8f33d4e
carellihoula <carellihoula@gmail.com>
2023-05-28 08:20:51 +0200

    premier envoi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -8,3 +11,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'consulting';
 }
+
+
